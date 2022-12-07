6f8aa7ed00882d2fe15814f2abab4e8d064ca906
Mohamed Wishah <mvishah@gmail.com>
2022-12-07 09:31:18 +0500

    Add model,migration,seeder,controller,form-requests

---
+++ app/Http/Controllers/BreedController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreBreedRequest;
+use App\Http\Requests\UpdateBreedRequest;
+use App\Models\Breed;
+
+class BreedController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreBreedRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreBreedRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Breed  $breed
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Breed $breed)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Breed  $breed
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Breed $breed)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateBreedRequest  $request
+     * @param  \App\Models\Breed  $breed
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateBreedRequest $request, Breed $breed)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Breed  $breed
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Breed $breed)
+    {
+        //
+    }
+}
